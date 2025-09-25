cde41a6dafac0dc6b7372ed7cb68de35fed61c7f
Trieu Tuan Thanh <trieutuanthanh2611@gmail.com>
2025-09-26 00:56:44 +0700

    Add FivePerLine.java

---
+++ HW1/FivePerLine.java
@@ -0,0 +1,11 @@
+public class FivePerLine {
+    public static void main(String[] args) {
+        for (int i = 1000; i <= 2000; i++) {
+            System.out.print(i + " ");
+
+            if ((i - 999) % 5 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
